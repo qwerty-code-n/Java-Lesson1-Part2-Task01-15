9dc11b86472c865c5e270c6c21d05dae9fc07cd2
qwerty-code-n <71195820+qwerty-code-n@users.noreply.github.com>
2020-09-28 14:49:38 +0300

    Add files via upload

---
+++ Task08.java
@@ -0,0 +1,46 @@
+package lesson01.part2;
+
+public class Task08 {
+
+    /**
+     * Создай 10 переменных типа Cat и 8 объектов типа Cat.
+     * <p>
+     * <p>
+     * Требования:
+     * 1. Программа не должна выводить текст на экран.
+     * 2. В методе main должно быть 10 переменных типа Cat.
+     * 3. В методе main 8 переменных должны быть проинициализированы.
+     * 4. В классе Cat не должно быть переменных.
+     * 5. В классе Cat не должно быть методов.
+     */
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        Cat _cat8 = new Cat();
+        Cat _cat7 = new Cat();
+        Cat _cat6 = new Cat();
+        Cat _cat5 = new Cat();
+        Cat _cat4 = new Cat();
+        Cat _cat3 = new Cat();
+        Cat _cat2 = new Cat();
+        Cat _cat1 = new Cat();
+
+        Cat cat1;
+        Cat cat2;
+        Cat cat3;
+        Cat cat4;
+        Cat cat5;
+        Cat cat6;
+        Cat cat7;
+        Cat _cat;
+        Cat cat9;
+        Cat cat10;
+
+
+        //напишите тут ваш код
+    }
+
+    public static class Cat {
+
+    }
+}
